39c771d16a3e5cc003b86b286ed1819d28a53e59
emeltec <emelgarejox@gmail.com>
2021-08-02 03:04:45 -0500

    Mensajes en español

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const urlBase = "http://localhost:3000";
+const URLBASE = "http://localhost:3000";
 
 fetch()
   .then(data => data.json())
@@ -25,10 +25,10 @@ function upload() {
 }
 
 function progressHandler(event) {
-    element("loadedtotal").innerHTML = "Uploaded " + event.loaded + " bytes of " + event.total;
+    element("loadedtotal").innerHTML = "Cargado " + event.loaded + " bytes de " + event.total;
     var percent = (event.loaded / event.total) * 100;
     element("progressBar").value = Math.round(percent);
-    element("status").innerHTML = Math.round(percent) + "% uploaded... please wait";
+    element("status").innerHTML = Math.round(percent) + "% subiendo... por favor espere";
     if(percent == 100){
         getFiles();
     }
@@ -40,11 +40,11 @@ function completeHandler(event) {
 }
 
 function errorHandler(event) {
-    element("status").innerHTML = "Upload Failed";
+    element("status").innerHTML = "Subida fallida";
 }
 
 function abortHandler(event) {
-    element("status").innerHTML = "Upload Aborted";
+    element("status").innerHTML = "Subida cancelada";
 }
 
 
@@ -57,7 +57,7 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 const getFiles = () => {
-    fetch(urlBase+'/files')
+    fetch(URLBASE+'/files')
     .then(data => data.json())
     .then(response => {
         let files = response.files;
@@ -68,7 +68,7 @@ const getFiles = () => {
 
 const deleteFile = (fileName) => {
     console.log(fileName)
-    fetch(urlBase+'/files/'+fileName)
+    fetch(URLBASE+'/files/'+fileName)
     .then(data => data.json())
     .then(response => {
         console.log(response);
@@ -81,7 +81,7 @@ const renderFiles = (files) => {
     files.forEach(file => {
         filesHTML += `
         <div class="file">
-            <img src="${urlBase}/${file}" width="300" />
+            <img src="${URLBASE}/${file}" width="300" />
             <div>
                 <span>Name: ${file} </span>
                 <button type="button" onclick="deleteFile('${file}')">Eliminar</button>
